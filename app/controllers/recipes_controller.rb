1fcbeacae3923ff3a2651dac51206454ae66a452
Tucker Steil <tuckersteil@gmail.com>
2023-01-20 14:31:34 -0800

    commit tree a86f1a0f9f66e4d2fa9ac3a5f484c0fcf625f8c7

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,25 @@
+class RecipesController < ApplicationController
+    before_action :authorize
+
+    def index
+        render json: Recipe.all, include: 'user', status: :created
+    end
+
+    def create
+        recipe = Recipe.create!(title: params[:title], instructions: params[:instructions], minutes_to_complete: params[:minutes_to_complete], user_id: session[:user_id])
+        render json: recipe, status: :created
+    rescue ActiveRecord::RecordInvalid => invalid
+        render json: { errors: [invalid.record.errors] }, status: :unprocessable_entity
+    end
+
+    private
+    
+    def authorize
+        return render json: { errors: ["Unauthorized"] }, status: :unauthorized unless session.include? :user_id
+    end
+
+    def recipe_params
+        params.permit(:title, :instructions, :minutes_to_complete)
+    end
+end
+
